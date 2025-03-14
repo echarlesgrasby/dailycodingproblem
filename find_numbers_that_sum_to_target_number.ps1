53505ac117a1a0fa9788afc79e72053f920f5d4b
Eric Charles Grasby, MSIQ <ericcgrasby@gmail.com>
2025-03-14 09:50:41 -0500

    Add file for "Sum to Target Number"
    The problem posed for this script is shown below: 
    
    This problem was recently asked by Google.
    
    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
    
    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.

---
+++ find_numbers_that_sum_to_target_number.ps1
@@ -0,0 +1,39 @@
+﻿$list_of_numbers = New-Object System.Collections.ArrayList;
+$list_of_numbers.Add(10) | Out-Null
+$list_of_numbers.Add(15) | Out-Null
+$list_of_numbers.Add(20) | Out-Null
+$list_of_numbers.Add(-3) | Out-Null
+
+$target_number = 17
+
+$new_list = New-Object System.Collections.ArrayList;
+
+function determine_objective(){
+    
+    #Have we determined that a match exists?
+    $found_flag = $false
+
+    #Short circuit a return for the empty set
+    if($list_of_numbers.Count -eq 0){
+        return $false
+    }
+
+    #iterate over the set- find the 2nd part of the pair
+    foreach($i in $list_of_numbers){
+        $new_list.Add(($target_number - $i)) | Out-Null
+    }
+
+    #find the 1st part of the pair
+    foreach($i in $new_list){
+        $other_item = $target_number - $i
+        if($other_item -in $list_of_numbers -and ($i -in $list_of_numbers)){
+            $found_flag = $true
+        }
+    }
+
+    #return the found flag
+    return $found_flag
+}
+
+$match_exists = determine_objective;
+"$match_exists"
